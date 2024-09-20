cef4574190ad468e3ba27e11dbb54a73aba5b181
ManifoldFR <wilson.jallet@polytechnique.org>
2024-09-20 12:52:04 +0200

    Add Device.cpp

---
+++ src/candlewick/core/Device.cpp
@@ -0,0 +1,15 @@
+#include "Device.h"
+#include <SDL3/SDL_log.h>
+
+namespace candlewick {
+
+Device::Device(SDL_GPUShaderFormat format_flags, bool debug_mode)
+    : _device(NULL) {
+  _device = SDL_CreateGPUDevice(format_flags, debug_mode, NULL);
+  if (!_device)
+    SDL_Log("CreateGPUDevice failed");
+  driver = SDL_GetGPUDeviceDriver(_device);
+  SDL_Log("Device driver: %s", driver);
+}
+
+} // namespace candlewick
---
+++ src/candlewick/core/Device.h
@@ -0,0 +1,22 @@
+#include <SDL3/SDL_gpu.h>
+
+namespace candlewick {
+
+struct Device {
+
+  Device(SDL_GPUShaderFormat format_flags, bool debug_mode = false);
+
+  operator SDL_GPUDevice *() { return _device; }
+  operator bool() const { return _device; }
+
+  void destroy() {
+    SDL_DestroyGPUDevice(_device);
+    _device = nullptr;
+  }
+
+private:
+  SDL_GPUDevice *_device;
+  const char *driver;
+};
+
+} // namespace candlewick
