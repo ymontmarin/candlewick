e2832439b3f85a33e5d4383b0a4847cc7dc78788
ManifoldFR <wilson.jallet@polytechnique.org>
2024-09-26 00:40:38 +0200

    somewhat working upload of vertices

--- assets/VertexColor.vert
+++ assets/VertexColor.vert
@@ -1,18 +1,15 @@
 #version 450
 
-layout(location=0)
-in vec4 position;
-
-layout(location=1)
-in vec4 color;
+layout(location=0) in vec3 position;
+layout(location=1) in vec4 color;
 
 layout(location=0) out vec4 interpColor;
 
-const mat4 cam_M = mat4(1.0);
 
 void main() {
+    mat4 cam_M = mat4(1.0);
 
-    gl_Position = cam_M * position;
+    gl_Position = cam_M * vec4(position, 1.0);
 
     interpColor = color;
 }
--- tests/Cube.cpp
+++ tests/Cube.cpp
@@ -3,10 +3,10 @@
 #include "candlewick/core/MeshLayout.h"
 #define SDL_GPU_SHADERCROSS_IMPLEMENTATION
 #include "SDL_gpu_shadercross.h"
-#include <array>
+#include <Eigen/Core>
 
-using float3 = std::array<float, 3>;
-using float4 = std::array<float, 4>;
+using float3 = Eigen::Vector3f;
+using float4 = Eigen::Vector4f;
 struct Vertex {
   float3 pos;
   float4 col;
@@ -14,19 +14,41 @@ struct Vertex {
 
 float4 red = {1.f, 0., 0., 1.};
 float4 green = {0.f, 1., 0., 1.};
+float4 blue = {0.f, 0., 1., 1.};
 
 // clang-format off
 Vertex vertexData[] = {
-  {{-1., -1., -1.}, red},
-  {{-1., +1., -1.}, red},
-  {{+1., +1., -1.}, green},
-  {{+1., -1., -1.}, red},
-  {{+1., -1., +1.}, red},
-  {{-1., -1., +1.}, red},
-  {{-1., +1., +1.}, red},
-  {{+1., +1., +1.}, green},
+  // f1
+  {{0., 0., 0.}, red},
+  {{0., 1., 0.}, green},
+  {{1., 1., 0.}, blue},
+  // f2
+  {{1., 1., 0.}, blue},
+  {{1., 0., 0.}, (blue + red) / 2},
+  {{0., 0., 0.}, red},
+  // {{+1., -1., +1.}, red},
+  // {{-1., -1., +1.}, red},
+  // {{-1., +1., +1.}, red},
+  // {{+1., +1., +1.}, green},
 };
 // clang-format on
+
+static Eigen::Matrix4f lookAt(const float3 &eye, const float3 &center,
+                              const float3 &up) {
+  Eigen::Matrix4f mat;
+  mat.setZero();
+  mat(3, 3) = 1.0;
+  float3 z = eye - center;
+  z.normalize();
+  float3 y = up;
+  float3 x = y.cross3(x);
+  y.normalize();
+  x.normalize();
+  mat.block<3, 1>(0, 0) = x;
+  mat.block<3, 1>(0, 1) = y;
+  mat.block<3, 1>(0, 2) = z;
+  return mat;
+}
 
 struct State {
   SDL_GPUBuffer *buf_vertex;
@@ -42,7 +64,7 @@ int main() {
   SDL_ShaderCross_Init();
   Device device{SDL_ShaderCross_GetSPIRVShaderFormats()};
 
-  SDL_Window *window = SDL_CreateWindow(__FILE_NAME__, 1280, 480, 0);
+  SDL_Window *window = SDL_CreateWindow(__FILE_NAME__, 1280, 720, 0);
   if (!SDL_ClaimWindowForGPUDevice(device, window)) {
     SDL_Log("Error: %s", SDL_GetError());
     return 1;
@@ -57,7 +79,7 @@ int main() {
 
   MeshLayout mesh_layout;
   mesh_layout.addBinding(0, sizeof(Vertex))
-      .addAttribute(0, 0, SDL_GPU_VERTEXELEMENTFORMAT_FLOAT4,
+      .addAttribute(0, 0, SDL_GPU_VERTEXELEMENTFORMAT_FLOAT3,
                     offsetof(Vertex, pos))
       .addAttribute(1, 0, SDL_GPU_VERTEXELEMENTFORMAT_FLOAT4,
                     offsetof(Vertex, col));
@@ -78,11 +100,11 @@ int main() {
       .size = sizeof(vertexData),
       .props = 0,
   };
-  SDL_GPUTransferBuffer *trans_buffer =
+  SDL_GPUTransferBuffer *transfer_buffer =
       SDL_CreateGPUTransferBuffer(device, &transfer_buffer_desc);
-  void *_map = SDL_MapGPUTransferBuffer(device, trans_buffer, false);
+  void *_map = SDL_MapGPUTransferBuffer(device, transfer_buffer, false);
   SDL_memcpy(_map, vertexData, sizeof(vertexData));
-  SDL_UnmapGPUTransferBuffer(device, trans_buffer);
+  SDL_UnmapGPUTransferBuffer(device, transfer_buffer);
 
   SDL_GPUCommandBuffer *cmdbuf;
   // Upload data to buffer
@@ -90,7 +112,7 @@ int main() {
     cmdbuf = SDL_AcquireGPUCommandBuffer(device);
     SDL_GPUCopyPass *copy_pass = SDL_BeginGPUCopyPass(cmdbuf);
     SDL_GPUTransferBufferLocation src_location;
-    src_location.transfer_buffer = trans_buffer;
+    src_location.transfer_buffer = transfer_buffer;
     src_location.offset = 0;
 
     SDL_GPUBufferRegion dst_location;
@@ -102,7 +124,7 @@ int main() {
     SDL_EndGPUCopyPass(copy_pass);
     SDL_SubmitGPUCommandBuffer(cmdbuf);
   }
-  SDL_ReleaseGPUTransferBuffer(device, trans_buffer);
+  SDL_ReleaseGPUTransferBuffer(device, transfer_buffer);
   // Pipeline
 
   SDL_GPUColorTargetDescription color_desc;
@@ -114,17 +136,25 @@ int main() {
       .fragment_shader = fragmentShader,
       .vertex_input_state = mesh_layout.toVertexInputState(),
       .primitive_type = SDL_GPU_PRIMITIVETYPE_TRIANGLELIST,
-      .depth_stencil_state{
-          .compare_op = SDL_GPU_COMPAREOP_LESS_OR_EQUAL,
-          .enable_depth_test = true,
-          .enable_depth_write = true,
-      },
-      .target_info{
-          .color_target_descriptions = &color_desc, .num_color_targets = 1,
-          //  .depth_stencil_format = SDL_GPU_TEXTUREFORMAT_D24_UNORM,
-          //  .has_depth_stencil_target = false,
-      },
+      // .depth_stencil_state{
+      //     .compare_op = SDL_GPU_COMPAREOP_LESS_OR_EQUAL,
+      //     .enable_depth_test = true,
+      //     .enable_depth_write = true,
+      // },
+      .rasterizer_state =
+          {
+              .fill_mode = SDL_GPU_FILLMODE_FILL,
+              .front_face = SDL_GPU_FRONTFACE_COUNTER_CLOCKWISE,
+          },
+      .target_info =
+          {
+              .color_target_descriptions = &color_desc, .num_color_targets = 1,
+              //  .depth_stencil_format = SDL_GPU_TEXTUREFORMAT_D24_UNORM,
+              //  .has_depth_stencil_target = false,
+          },
       .props = 0};
+  pipeline_desc.rasterizer_state.front_face =
+      SDL_GPU_FRONTFACE_COUNTER_CLOCKWISE;
   pipeline_desc.rasterizer_state.fill_mode = SDL_GPU_FILLMODE_FILL;
   SDL_GPUGraphicsPipeline *pipeline =
       SDL_CreateGPUGraphicsPipeline(device, &pipeline_desc);
@@ -148,7 +178,6 @@ int main() {
     vertex_binding.offset = 0;
     swapchain = SDL_AcquireGPUSwapchainTexture(cmdbuf, window, &w, &h);
     if (swapchain) {
-      SDL_Log("Acquired command buffer and swapchain.");
       SDL_GPUColorTargetInfo ctinfo{.texture = swapchain,
                                     .clear_color = SDL_FColor{},
                                     .load_op = SDL_GPU_LOADOP_CLEAR,
@@ -158,16 +187,16 @@ int main() {
       render_pass = SDL_BeginGPURenderPass(cmdbuf, &ctinfo, 1, NULL);
       SDL_BindGPUGraphicsPipeline(render_pass, pipeline);
       SDL_BindGPUVertexBuffers(render_pass, 0, &vertex_binding, 1);
-      SDL_DrawGPUPrimitives(render_pass, sizeof(vertexData) / sizeof(Vertex), 1,
-                            0, 0);
+      constexpr auto vertexCount = std::size(vertexData);
+      SDL_DrawGPUPrimitives(render_pass, vertexCount, 1, 0, 0);
       SDL_EndGPURenderPass(render_pass);
     }
     SDL_SubmitGPUCommandBuffer(cmdbuf);
   };
 
-  Render();
-  Render();
-  Render();
+  for (int i = 0; i < 200; i++) {
+    Render();
+  }
 
   SDL_ReleaseGPUGraphicsPipeline(device, pipeline);
 
