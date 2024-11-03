d4899324360ba40a2f34ce775bf1733f71b10390
ManifoldFR <wilson.jallet@polytechnique.org>
2024-11-03 16:34:49 +0100

    core:  implement Mesh

--- src/candlewick/core/Mesh.cpp
+++ src/candlewick/core/Mesh.cpp
@@ -1,1 +1,39 @@
-#include "candlewick/core/Mesh.h"
+#include "Mesh.h"
+#include "MeshLayout.h"
+#include "./errors.h"
+
+namespace candlewick {
+
+Mesh::Mesh(MeshLayout layout) : _layout(std::move(layout)) {
+  const Uint32 count = _layout.toVertexInputState().num_vertex_buffers;
+  vertexBuffers.resize(count);
+  vertexBufferOffsets.resize(count);
+}
+
+std::size_t Mesh::addVertexBufferImpl(Uint32 slot, SDL_GPUBuffer *buffer,
+                                      Uint64 offset) {
+  auto max = _layout.toVertexInputState().num_vertex_buffers;
+  for (std::size_t i = 0; i < max; i++) {
+    if (_layout.toVertexInputState().vertex_buffer_descriptions[i].slot ==
+        slot) {
+      vertexBuffers[i] = buffer;
+      vertexBufferOffsets[i] = offset;
+      return i;
+    }
+  }
+  CDW_UNREACHABLE_ASSERT("Binding not found in vertex buffer.");
+  return ~std::size_t{};
+}
+
+Mesh &Mesh::addVertexBuffer(Uint32 slot, SDL_GPUBuffer *buffer, Uint64 offset) {
+  addVertexBufferImpl(slot, buffer, offset);
+  return *this;
+}
+
+Mesh &Mesh::setIndexBuffer(SDL_GPUBuffer *buffer, Uint64 offset) {
+  indexBuffer = buffer;
+  indexBufferOffset = offset;
+  return *this;
+}
+
+} // namespace candlewick
--- src/candlewick/core/Mesh.h
+++ src/candlewick/core/Mesh.h
@@ -1,9 +1,41 @@
 #pragma once
 
-#include <SDL3/SDL_gpu.h>
+#include "Core.h"
+#include "MeshLayout.h"
 
 namespace candlewick {
 
-struct Mesh {};
+/// \brief Convenience class for handling meshes: manages vertex and index
+/// buffers.
+struct Mesh {
+  explicit Mesh(MeshLayout layout);
+
+  Mesh(const Mesh &) = delete;
+  Mesh(Mesh &&) noexcept = default;
+
+  Mesh &operator=(const Mesh &) = delete;
+  Mesh &operator=(Mesh &&) noexcept = delete;
+
+  const MeshLayout &layout() const { return _layout; }
+
+  Mesh &addVertexBuffer(Uint32 slot, SDL_GPUBuffer *buffer, Uint64 offset);
+  Mesh &setIndexBuffer(SDL_GPUBuffer *buffer, Uint64 offset);
+
+  bool isIndexed() const { return indexBuffer; }
+
+private:
+  /// Add a vertex buffer corresponding to binding slot @p binding,
+  /// using the pre-allocated @p buffer.
+  std::size_t addVertexBufferImpl(Uint32 slot, SDL_GPUBuffer *buffer,
+                                  Uint64 offset);
+
+  MeshLayout _layout;
+
+public:
+  std::vector<SDL_GPUBuffer *> vertexBuffers;
+  std::vector<Uint64> vertexBufferOffsets;
+  SDL_GPUBuffer *indexBuffer;
+  Uint64 indexBufferOffset;
+};
 
 } // namespace candlewick
