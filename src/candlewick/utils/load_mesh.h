1e007b948fe01f71ebe0d978567b33fab751a845
ManifoldFR <wilson.jallet@polytechnique.org>
2024-11-17 19:56:55 +0100

    load_mesh.h : add docstrings

--- src/candlewick/utils/load_mesh.h
+++ src/candlewick/utils/load_mesh.h
@@ -6,8 +6,11 @@ namespace candlewick {
 
 struct MeshData;
 
+/// Return codes for \ref loadSceneMeshes().
 enum class LoadMeshReturn { OK, FailedToLoad, NoMeshes };
 
+/// \brief Load the meshes from the given path.
+/// This is implemented using the assimp library.
 LoadMeshReturn loadSceneMeshes(const char *path,
                                std::vector<MeshData> &meshData);
 } // namespace candlewick
