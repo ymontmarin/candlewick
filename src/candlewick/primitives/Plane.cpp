355318fa4b2400938e2e711768077ecd30cc957f
ManifoldFR <wilson.jallet@polytechnique.org>
2024-11-25 14:49:17 +0100

    Plane.cpp : add loadPlaneTiled

--- src/candlewick/primitives/Plane.cpp
+++ src/candlewick/primitives/Plane.cpp
@@ -1,7 +1,12 @@
 #include "Plane.h"
+#include "../utils/MeshTransforms.h"
 
 namespace candlewick {
 
+// 3——1
+// │ /│
+// │/ │
+// 2——0
 MeshData::Vertex vertexData[]{{{1.0f, -1.0f, 0.0f}, {0.0f, 0.0f, 1.0f}},
                               {{1.0f, 1.0f, 0.0f}, {0.0f, 0.0f, 1.0f}},
                               {{-1.0f, -1.0f, 0.0f}, {0.0f, 0.0f, 1.0f}},
@@ -9,6 +14,33 @@ MeshData::Vertex vertexData[]{{{1.0f, -1.0f, 0.0f}, {0.0f, 0.0f, 1.0f}},
 
 MeshDataView loadPlane() {
   return {SDL_GPU_PRIMITIVETYPE_TRIANGLESTRIP, vertexData};
+}
+
+MeshData loadPlaneTiled(float scale, Uint32 xrepeat, Uint32 yrepeat,
+                        bool centered) {
+  MeshData dataOwned = generateIndices(toOwningMeshData(loadPlane()));
+  {
+    // normalize to (-1,-1) -- (1,1)
+    const Eigen::Translation3f tr{0.5, 0.5};
+    apply3DTransformInPlace(dataOwned, Eigen::Affine3f(tr).scale(0.5));
+  }
+  std::vector<MeshData> meshes;
+  meshes.reserve(xrepeat * yrepeat);
+  for (Sint32 i = 0; i < Sint32(xrepeat); i++) {
+    for (Sint32 j = 0; j < Sint32(yrepeat); j++) {
+      MeshData &m = meshes.emplace_back(toOwningMeshData(dataOwned));
+      const Eigen::Translation3f tr{i * scale, j * scale, 0.};
+      apply3DTransformInPlace(m, Eigen::Affine3f(tr).scale(scale));
+    }
+  }
+  MeshData out = mergeMeshes(meshes);
+  if (centered) {
+    float xc = 0.5f * scale * xrepeat;
+    float yc = 0.5f * scale * yrepeat;
+    const Eigen::Translation3f center{-xc, -yc, 0.f};
+    apply3DTransformInPlace(out, Eigen::Affine3f{center});
+  }
+  return out;
 }
 
 } // namespace candlewick
--- src/candlewick/primitives/Plane.h
+++ src/candlewick/primitives/Plane.h
@@ -6,4 +6,7 @@ namespace candlewick {
 
 MeshDataView loadPlane();
 
-}
+MeshData loadPlaneTiled(float scale, Uint32 xrepeat, Uint32 yrepeat,
+                        bool centered = true);
+
+} // namespace candlewick
