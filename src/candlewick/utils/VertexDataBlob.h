dff38582e59aaf5799eb2ca8a3b09908d57c796c
ManifoldFR <wilson.jallet@polytechnique.org>
2024-12-01 23:47:02 +0100

    Add VertexDataBlob
    and test

---
+++ src/candlewick/utils/VertexDataBlob.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include "../core/MeshLayout.h"
+#include <span>
+#include <vector>
+#include <SDL3/SDL_assert.h>
+
+namespace candlewick {
+
+/// \brief A type-erased container for vertex data.
+class VertexDataBlob {
+private:
+  std::vector<char> m_data;
+  MeshLayout m_layout;
+  Uint64 m_actualSize;
+
+public:
+  VertexDataBlob(std::vector<char> data, MeshLayout layout)
+      : m_data(data), m_layout(layout),
+        m_actualSize(data.size() / layout.vertexSize()) {}
+
+  template <IsVertexType V>
+  VertexDataBlob(const std::vector<V> &vertices)
+      : m_data(
+            reinterpret_cast<const char *>(vertices.data()),
+            reinterpret_cast<const char *>(vertices.data() + vertices.size())),
+        m_layout(meshLayoutFor<V>()), m_actualSize(vertices.size()) {
+    // compile-time check
+    static_assert(meshLayoutFor<V>().vertexSize() == sizeof(V));
+  }
+
+  template <typename U> std::span<const U> viewAs() const {
+    const U *begin = reinterpret_cast<const U *>(m_data.data());
+    return std::span<const U>(begin, size());
+  }
+
+  template <typename U> std::span<U> viewAs() {
+    U *begin = reinterpret_cast<U *>(m_data.data());
+    return std::span<U>(begin, size());
+  }
+
+  /// \brief Access an attribute. Use this when the underlying vertex data type
+  /// is unknown.
+  template <typename T>
+  T &getAttribute(const Uint64 vertexId, const SDL_GPUVertexAttribute &attr) {
+    SDL_assert(vertexId < m_actualSize);
+    const Uint32 vtxSize = m_layout.vertexSize();
+    T *ptr =
+        reinterpret_cast<T *>(m_data.data() + vertexId * vtxSize + attr.offset);
+    return *ptr;
+  }
+
+  template <typename T> T &getAttribute(const Uint64 vertexId, size_t attrLoc) {
+    SDL_assert(attrLoc < m_layout.numAttributes());
+    const auto &attr = m_layout.vertex_attributes[attrLoc];
+    SDL_assert(attr.location == attrLoc);
+    return this->getAttribute<T>(vertexId, attr);
+  }
+
+  /// \brief Raw ptr to underlying data.
+  char *data() { return m_data.data(); }
+  /// \copybrief data()
+  const char *data() const { return m_data.data(); }
+
+  const MeshLayout &layout() const { return m_layout; }
+
+  Uint64 size() const { return m_actualSize; }
+};
+
+} // namespace candlewick
---
+++ tests/TestVertexBlob.cpp
@@ -0,0 +1,85 @@
+#include "candlewick/utils/MeshData.h"
+#include "candlewick/utils/VertexDataBlob.h"
+#include <gtest/gtest.h>
+
+using namespace candlewick;
+
+bool operator==(const DefaultVertex &lhs, const DefaultVertex &rhs) {
+  return (lhs.pos == rhs.pos) && (lhs.normal == rhs.normal) &&
+         (lhs.color == rhs.color);
+}
+
+GTEST_TEST(TestErasedBlob, default_vertex) {
+  std::vector<DefaultVertex> vertexData;
+  Uint64 size = 10;
+  for (Uint64 i = 0; i < size; i++) {
+    vertexData.push_back(
+        {Float3::Random(), Float3::Zero(), 0.5f * Float4::Random()});
+  }
+
+  VertexDataBlob blob(vertexData);
+  EXPECT_EQ(blob.size(), size);
+
+  std::span<const DefaultVertex> view = blob.viewAs<DefaultVertex>();
+  EXPECT_EQ(view.size(), size);
+
+  for (Uint64 i = 0; i < size; i++) {
+    EXPECT_TRUE(view[i] == vertexData[i]);
+    EXPECT_TRUE(vertexData[i].pos == blob.getAttribute<Float3>(i, 0));
+    EXPECT_TRUE(vertexData[i].normal == blob.getAttribute<Float3>(i, 1));
+    EXPECT_TRUE(vertexData[i].color == blob.getAttribute<Float4>(i, 2));
+  }
+}
+
+struct CustomVertex {
+  GpuVec4 pos;
+  alignas(16) GpuVec3 color;
+  alignas(16) GpuVec2 uv;
+};
+static_assert(IsVertexType<CustomVertex>, "Invalid vertex type.");
+
+template <> struct VertexTraits<CustomVertex> {
+  constexpr static auto layout() {
+    return MeshLayout{}
+        .addBinding(0, sizeof(CustomVertex))
+        .addAttribute(0, 0, SDL_GPU_VERTEXELEMENTFORMAT_FLOAT4,
+                      offsetof(CustomVertex, pos))
+        .addAttribute(1, 0, SDL_GPU_VERTEXELEMENTFORMAT_FLOAT3,
+                      offsetof(CustomVertex, color))
+        .addAttribute(2, 0, SDL_GPU_VERTEXELEMENTFORMAT_FLOAT2,
+                      offsetof(CustomVertex, uv));
+  }
+};
+
+bool operator==(const CustomVertex &lhs, const CustomVertex &rhs) {
+  return (lhs.pos == rhs.pos) && (lhs.color == rhs.color) && (lhs.uv == rhs.uv);
+}
+
+GTEST_TEST(TestErasedBlob, custom_vertex) {
+  auto layout = meshLayoutFor<CustomVertex>();
+  EXPECT_EQ(sizeof(CustomVertex), layout.vertexSize());
+
+  std::vector<CustomVertex> vertexData;
+  Uint64 size = 3;
+  for (Uint64 i = 0; i < size; i++) {
+    vertexData.push_back({Float4::Random(), Float3::Zero(), Float2::Random()});
+  }
+
+  VertexDataBlob blob(vertexData);
+  EXPECT_EQ(blob.size(), size);
+
+  std::span<const CustomVertex> view = blob.viewAs<CustomVertex>();
+  EXPECT_EQ(view.size(), size);
+
+  for (Uint64 i = 0; i < size; i++) {
+    EXPECT_TRUE(view[i] == vertexData[i]);
+    EXPECT_TRUE(vertexData[i].pos == blob.getAttribute<Float4>(i, 0));
+    EXPECT_TRUE(vertexData[i].color == blob.getAttribute<Float3>(i, 1));
+    EXPECT_TRUE(vertexData[i].uv == blob.getAttribute<Float2>(i, 2));
+  }
+}
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
