178f5d67384f242ec65bc262ced3f336c4f69a0a
ManifoldFR <wilson.jallet@polytechnique.org>
2024-12-02 19:52:38 +0100

    Slightly rework PixelFormatConversion.cpp

--- src/candlewick/utils/PixelFormatConversion.cpp
+++ src/candlewick/utils/PixelFormatConversion.cpp
@@ -2,8 +2,8 @@
 
 namespace candlewick {
 
-void bgraToRgbaConvert(const Uint32 *bgraPixels, Uint32 *rgbaPixels,
-                       Uint32 pixelCount) {
+void bgraToRgbaConvert_impl(const Uint32 *bgraPixels, Uint32 *rgbaPixels,
+                            Uint32 pixelCount) {
   // define appropriate masks for BGRA format
   Uint32 red_mask = 0x00FF0000;
   Uint32 green_mask = 0x0000FF00;
@@ -11,12 +11,17 @@ void bgraToRgbaConvert(const Uint32 *bgraPixels, Uint32 *rgbaPixels,
   Uint32 alpha_mask = 0xFF000000;
 
   for (Uint32 i = 0; i < pixelCount; ++i) {
-    Uint32 pixel = bgraPixels[i];                 // Load BGRA pixel
+    Uint32 pixel = bgraPixels[i];
     rgbaPixels[i] = ((pixel & red_mask) >> 16) |  // Extract Red
                     ((pixel & green_mask)) |      // Keep Green
                     ((pixel & blue_mask) << 16) | // Extract Blue
                     (pixel & alpha_mask);         // Keep Alpha
   }
+}
+
+void bgraToRgbaConvert(const Uint32 *bgraPixels, Uint32 *rgbaPixels,
+                       Uint32 pixelCount) {
+  bgraToRgbaConvert_impl(bgraPixels, rgbaPixels, pixelCount);
 }
 
 } // namespace candlewick
