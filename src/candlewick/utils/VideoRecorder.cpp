da0af42ca87ce771725b1a35f1bd389d015ac834
ManifoldFR <wilson.jallet@polytechnique.org>
2024-12-08 17:29:32 +0100

    Remove pimpl'd State struct
    Implement VideoRecorder::writeFrame()

--- src/candlewick/utils/VideoRecorder.cpp
+++ src/candlewick/utils/VideoRecorder.cpp
@@ -1,50 +1,34 @@
 #include "VideoRecorder.h"
 
-extern "C" {
-#include <libavformat/avformat.h>
-#include <libavcodec/avcodec.h>
-#include <libswscale/swscale.h>
-}
-
 #include <SDL3/SDL_log.h>
 #include <SDL3/SDL_filesystem.h>
 #include <format>
 
 namespace candlewick::media {
 
-struct VideoRecorder::State {
-  AVFormatContext *formatContext = nullptr;
-  const AVCodec *codec = nullptr;
-  AVCodecContext *codecContext = nullptr;
-  AVStream *videoStream = nullptr;
-  SwsContext *swsContext = nullptr;
-  AVFrame *frame = nullptr;
-  AVPacket *packet = nullptr;
+VideoRecorder::~VideoRecorder() noexcept {
+  av_write_trailer(formatContext);
+  if (codecContext)
+    avcodec_free_context(&codecContext);
+  if (formatContext->pb)
+    avio_closep(&formatContext->pb);
 
-  ~State() {
-    av_write_trailer(formatContext);
-    if (codecContext)
-      avcodec_free_context(&codecContext);
-    if (formatContext->pb)
-      avio_closep(&formatContext->pb);
+  avformat_free_context(formatContext);
 
-    avformat_free_context(formatContext);
-
-    if (frame)
-      av_frame_free(&frame);
-    if (packet)
-      av_packet_free(&packet);
-  }
-};
+  if (frame)
+    av_frame_free(&frame);
+  if (packet)
+    av_packet_free(&packet);
+}
 
 VideoRecorder::VideoRecorder(Uint32 width, Uint32 height,
                              const std::string &filename, Settings settings)
-    : m_width(width), m_height(height), m_state(new State()) {
+    : m_width(width), m_height(height) {
   avformat_network_init();
-  m_state->codec = avcodec_find_encoder(AV_CODEC_ID_H264);
+  codec = avcodec_find_encoder(AV_CODEC_ID_H264);
 
-  int ret = avformat_alloc_output_context2(&m_state->formatContext, nullptr,
-                                           nullptr, filename.c_str());
+  int ret = avformat_alloc_output_context2(&formatContext, nullptr, nullptr,
+                                           filename.c_str());
   char errbuf[AV_ERROR_MAX_STRING_SIZE]{0};
   if (ret < 0) {
     av_strerror(ret, errbuf, AV_ERROR_MAX_STRING_SIZE);
@@ -52,14 +36,12 @@ VideoRecorder::VideoRecorder(Uint32 width, Uint32 height,
     throw std::runtime_error(msg);
   }
 
-  m_state->videoStream =
-      avformat_new_stream(m_state->formatContext, m_state->codec);
+  videoStream = avformat_new_stream(formatContext, codec);
 
-  m_state->codecContext = avcodec_alloc_context3(m_state->codec);
-  auto codecContext = m_state->codecContext;
+  codecContext = avcodec_alloc_context3(codec);
 
-  codecContext->width = int(m_width);
-  codecContext->height = int(m_height);
+  codecContext->width = settings.outputWidth;
+  codecContext->height = settings.outputHeight;
   codecContext->pix_fmt = AV_PIX_FMT_YUV420P;
   codecContext->time_base = AVRational{1, settings.fps};
   codecContext->framerate = AVRational{settings.fps, 1};
@@ -67,47 +49,101 @@ VideoRecorder::VideoRecorder(Uint32 width, Uint32 height,
   codecContext->max_b_frames = 1;
   codecContext->bit_rate = settings.bit_rate;
 
-  ret = avcodec_parameters_from_context(m_state->videoStream->codecpar,
-                                        codecContext);
+  ret = avcodec_parameters_from_context(videoStream->codecpar, codecContext);
   if (ret < 0) {
     av_strerror(ret, errbuf, AV_ERROR_MAX_STRING_SIZE);
     throw std::runtime_error(
         std::format("Couldn't copy codec params: {:s}", errbuf));
   }
 
-  ret = avcodec_open2(codecContext, m_state->codec, nullptr);
+  ret = avcodec_open2(codecContext, codec, nullptr);
   if (ret < 0) {
     av_strerror(ret, errbuf, AV_ERROR_MAX_STRING_SIZE);
     throw std::runtime_error(std::format("Couldn't open codec: {:s}", errbuf));
   }
 
-  ret =
-      avio_open(&m_state->formatContext->pb, filename.c_str(), AVIO_FLAG_WRITE);
+  ret = avio_open(&formatContext->pb, filename.c_str(), AVIO_FLAG_WRITE);
   if (ret < 0) {
     av_strerror(ret, errbuf, AV_ERROR_MAX_STRING_SIZE);
     throw std::runtime_error(
         std::format("Couldn't open output stream: {:s}", errbuf));
   }
 
-  ret = avformat_write_header(m_state->formatContext, nullptr);
+  ret = avformat_write_header(formatContext, nullptr);
   if (ret < 0) {
     av_strerror(ret, errbuf, AV_ERROR_MAX_STRING_SIZE);
     throw std::runtime_error(
         std::format("Couldn't write output header: {:s}", errbuf));
   }
 
-  m_state->frame = av_frame_alloc();
-  m_state->packet = av_packet_alloc();
-  m_state->frame->format = codecContext->pix_fmt;
-  m_state->frame->width = codecContext->width;
-  m_state->frame->height = codecContext->height;
+  frame = av_frame_alloc();
+  packet = av_packet_alloc();
+  frame->format = codecContext->pix_fmt;
+  frame->width = codecContext->width;
+  frame->height = codecContext->height;
 
-  ret = av_frame_get_buffer(m_state->frame, 0);
+  ret = av_frame_get_buffer(frame, 0);
   if (ret < 0) {
     av_strerror(ret, errbuf, AV_ERROR_MAX_STRING_SIZE);
     throw std::runtime_error(
         std::format("Failed to allocate frame: {:s}", errbuf));
   }
+}
+
+void VideoRecorder::writeFrame(const Uint8 *data, size_t payloadSize,
+                               AVPixelFormat avPixelFormat) {
+  AVFrame *tmpFrame = av_frame_alloc();
+  tmpFrame->format = codecContext->pix_fmt;
+  tmpFrame->width = int(m_width);
+  tmpFrame->height = int(m_height);
+
+  int ret = av_frame_get_buffer(tmpFrame, 0);
+  char errbuf[AV_ERROR_MAX_STRING_SIZE]{0};
+  if (ret < 0) {
+    av_strerror(ret, errbuf, AV_ERROR_MAX_STRING_SIZE);
+    throw std::runtime_error(
+        std::format("Failed to allocate frame: {:s}", errbuf));
+  }
+
+  SDL_memcpy(tmpFrame->data[0], data, payloadSize);
+
+  swsContext =
+      sws_getContext(tmpFrame->width, tmpFrame->height, avPixelFormat,
+                     frame->width, frame->height, codecContext->pix_fmt,
+                     SWS_BILINEAR, nullptr, nullptr, nullptr);
+
+  frame->pts = m_frameCounter++;
+
+  sws_scale(swsContext, tmpFrame->data, tmpFrame->linesize, 0, m_height,
+            frame->data, frame->linesize);
+
+  ret = avcodec_send_frame(codecContext, frame);
+  if (ret < 0) {
+    av_frame_free(&tmpFrame);
+    sws_freeContext(swsContext);
+    throw std::runtime_error(std::format("Error sending frame {:s}", errbuf));
+  }
+
+  while (ret >= 0) {
+    ret = avcodec_receive_packet(codecContext, packet);
+    if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF) {
+      break;
+    }
+    if (ret < 0) {
+      av_frame_free(&tmpFrame);
+      sws_freeContext(swsContext);
+      throw std::runtime_error("Error receiving packet from encoder");
+    }
+
+    av_packet_rescale_ts(packet, codecContext->time_base,
+                         videoStream->time_base);
+    packet->stream_index = videoStream->index;
+    av_interleaved_write_frame(formatContext, packet);
+    av_packet_unref(packet);
+  }
+
+  av_frame_free(&tmpFrame);
+  sws_freeContext(swsContext);
 }
 
 } // namespace candlewick::media
--- src/candlewick/utils/VideoRecorder.h
+++ src/candlewick/utils/VideoRecorder.h
@@ -1,35 +1,54 @@
 #pragma once
 
-#include "../core/Core.h"
 #include <SDL3/SDL_gpu.h>
-#include <memory>
 #include <string>
+
+extern "C" {
+#include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+#include <libswscale/swscale.h>
+}
 
 namespace candlewick {
 namespace media {
 
 class VideoRecorder {
 private:
-  Uint32 m_width;
-  Uint32 m_height;
+  Uint32 m_width;  //< Width of incoming frames
+  Uint32 m_height; //< Height of incoming frames
   Uint32 m_frameCounter;
 
-  struct State;
-  std::unique_ptr<State> m_state;
+  AVFormatContext *formatContext = nullptr;
+  const AVCodec *codec = nullptr;
+  AVCodecContext *codecContext = nullptr;
+  AVStream *videoStream = nullptr;
+  SwsContext *swsContext = nullptr;
+  AVFrame *frame = nullptr;
+  AVPacket *packet = nullptr;
 
 public:
   struct Settings {
     int fps = 30;
     long bit_rate = 2500000u;
+    int outputWidth;
+    int outputHeight;
   };
 
   VideoRecorder(Uint32 width, Uint32 height, const std::string &filename,
                 Settings settings);
 
   VideoRecorder(Uint32 width, Uint32 height, const std::string &filename)
-      : VideoRecorder(width, height, filename, Settings{}) {}
+      : VideoRecorder(width, height, filename,
+                      Settings{
+                          .outputWidth = int(width),
+                          .outputHeight = int(height),
+                      }) {}
 
   Uint32 frameCounter() const { return m_frameCounter; }
+  void writeFrame(const Uint8 *data, size_t payloadSize,
+                  AVPixelFormat avPixelFormat);
+
+  ~VideoRecorder() noexcept;
 };
 
 } // namespace media
