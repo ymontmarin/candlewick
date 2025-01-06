9d0a9a2c8a595829a545be02770c00ce66086015
ManifoldFR <wilson.jallet@polytechnique.org>
2025-01-06 16:28:51 +0100

    Add Shape.h

---
+++ src/candlewick/core/Shape.h
@@ -0,0 +1,12 @@
+#include "candlewick/core/Mesh.h"
+#include "candlewick/utils/MaterialData.h"
+
+namespace candlewick {
+
+/// \brief Simple struct holding draw data for a set of meshes with materials.
+struct ShapeData {
+  std::vector<Mesh> meshes;
+  std::vector<PbrMaterialData> materials;
+  bool hasMaterials() const { return !materials.empty(); }
+};
+} // namespace candlewick
