ab49eef4d4096340b9cf15f7c71398b7448d3587
ManifoldFR <wilson.jallet@polytechnique.org>
2025-01-06 16:28:17 +0100

    LoadPinocchioGeometry : add docstrings

--- src/candlewick/multibody/LoadPinocchioGeometry.h
+++ src/candlewick/multibody/LoadPinocchioGeometry.h
@@ -7,9 +7,15 @@
 
 namespace candlewick::multibody {
 
+/// \brief Load an invidual Pinocchio GeometryObject's component geometries into
+/// an array of \c MeshData.
 void loadGeometryObject(const pin::GeometryObject &gobj,
                         std::vector<MeshData> &meshData);
 
+/// \brief Load the component geometries of a Pinocchio GeometryModel (e.g. the
+/// visual geometry) into an array of \c MeshData.
+/// \sa loadGeometryObject() (loads invidual \c pinocchio::GeometryObject
+/// objects).
 void loadGeometryModel(const pin::GeometryModel &model,
                        std::vector<MeshData> &meshData);
 
