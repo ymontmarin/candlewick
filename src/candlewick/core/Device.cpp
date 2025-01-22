3ed236fc80df4d38bf2bf3e02c9bdd8cde04c1f0
ManifoldFR <wilson.jallet@polytechnique.org>
2025-01-22 16:25:57 +0100

    core/Device : enhancements
    + add move operator
    + define move-ctor out of line
    + mark some functions and ctor noexcept
    + change some NULL to nullptr

--- src/candlewick/core/Device.cpp
+++ src/candlewick/core/Device.cpp
@@ -1,24 +1,26 @@
 #include "Device.h"
 #include "errors.h"
+
 #include <SDL3/SDL_log.h>
+#include <format>
 
 namespace candlewick {
 
 Device::Device(SDL_GPUShaderFormat format_flags, bool debug_mode)
-    : _device(NULL) {
-  _device = SDL_CreateGPUDevice(format_flags, debug_mode, NULL);
+    : _device(nullptr) {
+  _device = SDL_CreateGPUDevice(format_flags, debug_mode, nullptr);
   if (!_device) {
-    SDL_Log("CreateGPUDevice failed");
-    throw RAIIException(SDL_GetError());
+    throw RAIIException(
+        std::format("Failed to create GPU device: %s", SDL_GetError()));
   }
   driver = SDL_GetGPUDeviceDriver(_device);
   SDL_Log("Device driver: %s", driver);
 }
 
-void Device::destroy() {
+void Device::destroy() noexcept {
   if (_device)
     SDL_DestroyGPUDevice(_device);
-  _device = NULL;
+  _device = nullptr;
 }
 
 } // namespace candlewick
