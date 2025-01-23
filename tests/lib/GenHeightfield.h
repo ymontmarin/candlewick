d168bc818e7a9835e34514cba587d1d08fa6c4ad
ManifoldFR <wilson.jallet@polytechnique.org>
2025-01-23 23:51:01 +0100

    tests : add GenHeightfield.cpp Change libCommon to static library

--- tests/lib/GenHeightfield.h
+++ tests/lib/GenHeightfield.h
@@ -1,37 +1,11 @@
 #pragma once
 
-#include "PerlinNoise.h"
+#include <SDL3/SDL_stdinc.h>
 #include <coal/hfield.h>
 
 /// \brief Generate a heightfield using Perlin noise.
 /// \param seed Random seed
 /// \param nx Number of subdivisions in x-direction
 /// \param x_dim Full width of the heigthfield
-inline auto generatePerlinNoiseHeightfield(Uint32 seed, Uint32 nx,
-                                           float x_dim) {
-  PerlinNoise noise{seed};
-
-  const Uint32 ny = nx;
-  const float y_dim = x_dim;
-  Eigen::MatrixXf heights{nx, ny};
-  heights.setZero();
-
-  std::vector amplitudes = {0.1f, 0.3f};
-  float base = 2.f;
-  size_t num_octaves = amplitudes.size();
-
-  float x, y;
-  for (Uint32 i = 0; i < nx; i++) {
-    x = float(i) * x_dim;
-    for (Uint32 j = 0; j < ny; j++) {
-      y = float(j) * y_dim;
-      for (auto k = 0ul; k < num_octaves; k++) {
-        float w = powf(base, float(k));
-        heights(i, j) += amplitudes[k] * noise.noise(w * x, w * y);
-      }
-    }
-  }
-  assert(!heights.hasNaN());
-  return std::make_shared<coal::HeightField<coal::AABB>>(
-      x_dim, y_dim, heights.cast<double>());
-}
+std::shared_ptr<coal::HeightField<coal::AABB>>
+generatePerlinNoiseHeightfield(Uint32 seed, Uint32 nx, float x_dim);
