31953096b6d5390143d22fcf6fe0e219941585d6
ManifoldFR <wilson.jallet@polytechnique.org>
2025-01-28 13:36:51 +0100

    assets/shaders : argument rename some arguments in RenderDepth.frag

--- assets/shaders/src/depth_utils.glsl
+++ assets/shaders/src/depth_utils.glsl
@@ -1,9 +1,9 @@
 
-float linearizeDepth(float depth, float near, float far) {
+float linearizeDepth(float depth, float zNear, float zFar) {
     float z_ndc = 2.0 * depth - 1.0;
-    return (2.0 * near * far) / (far + near - z_ndc * (far - near));
+    return (2.0 * zNear * zFar) / (zFar + zNear - z_ndc * (zFar - zNear));
 }
 
-float linearizeDepthOrtho(float depth, float near, float far) {
-    return near + depth * (far - near);
+float linearizeDepthOrtho(float depth, float zNear, float zFar) {
+    return zNear + depth * (zFar - zNear);
 }
