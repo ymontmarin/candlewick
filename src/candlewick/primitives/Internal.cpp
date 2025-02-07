fabe0b51ca362441e1b9d55404b77c61adec44ca
ManifoldFR <wilson.jallet@polytechnique.org>
2025-02-07 18:16:11 +0100

    smooth face normal for the cone :)

--- src/candlewick/primitives/Internal.cpp
+++ src/candlewick/primitives/Internal.cpp
@@ -29,7 +29,7 @@ namespace detail {
   void ConeCylinderBuilder::addCone(Uint32 segments, float radius,
                                     float zBottom, float length) {
     const float angleIncrement = 2.f * constants::Pif / float(segments);
-    Uint32 base_centre_idx = Uint32(currentVertices());
+    const Uint32 base_centre_idx = Uint32(currentVertices());
 
     // BOTTOM
     addDisk(segments, radius, zBottom);
@@ -37,25 +37,34 @@ namespace detail {
     // CONE TIP
     // direction from (radius, 0) in the xz-plane to the tip at (0, length).
     // normal vector to updir points outward towards (x+,z+)
-    Float2 updir = {-radius, length};
+    Uint32 current_floor_start_idx = base_centre_idx + 1;
+    Float2 base_point{radius, zBottom};
+    const Float2 updir{-radius, length};
     Float2 base_normal = Float2{length, radius}.normalized();
-    Float3 tip_pos{0.f, 0.f, zBottom + length};
 
-    Uint32 tip_start_idx = Uint32(currentVertices());
-    for (Uint32 i = 0; i < segments; i++) {
-      float a = static_cast<float>(i) * angleIncrement;
-      float c = cosf(a);
-      float s = sinf(a);
-      add(tip_pos, {c * base_normal[0], s * base_normal[0], base_normal[1]});
-    }
-    for (Uint32 i = 0; i < segments; i++) {
-      const Uint32 off = base_centre_idx + 1;
-      Uint32 j = i + off;
-      addFace({
-          j,
-          (i != segments - 1) ? j + 1 : off,
-          tip_start_idx,
-      });
+    for (size_t i = 0; i < 2; i++) {
+
+      Uint32 next_floor_start_idx = Uint32(currentVertices());
+      for (Uint32 i = 0; i < segments; i++) {
+        float a = static_cast<float>(i) * angleIncrement;
+        float c = cosf(a);
+        float s = sinf(a);
+        Float3 pos{c * base_point[0], s * base_point[0], base_point[1]};
+        add(pos, {c * base_normal[0], s * base_normal[0], base_normal[1]});
+      }
+      for (Uint32 i = 0; i < segments; i++) {
+        const Uint32 offset = current_floor_start_idx;
+        Uint32 j = i + offset;
+        addFace({
+            j,
+            (i != segments - 1) ? j + 1 : offset,
+            next_floor_start_idx + i,
+        });
+      }
+
+      current_floor_start_idx = next_floor_start_idx;
+
+      base_point += updir;
     }
   }
 } // namespace detail
