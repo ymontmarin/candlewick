764189b9a3bfa3ea0a91ed9a38b7cf29e8869fdf
ManifoldFR <wilson.jallet@polytechnique.org>
2025-02-13 19:12:10 +0100

    add capsule

---
+++ src/candlewick/primitives/Capsule.cpp
@@ -0,0 +1,10 @@
+#include "Capsule.h"
+#include "Internal.h"
+
+namespace candlewick {
+
+MeshData loadCapsuleSolid(Uint32 rings, Uint32 segments, float length) {
+  detail::ConeCylinderBuilder builder;
+}
+
+} // namespace candlewick
---
+++ src/candlewick/primitives/Capsule.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "../utils/MeshData.h"
+
+namespace candlewick {
+
+/// Load a capsule primitive.
+/// \note This will have a unit radius, for simplicity
+MeshData loadCapsuleSolid(Uint32 rings, Uint32 segments, float length);
+
+} // namespace candlewick
