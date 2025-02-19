2fc8318de1152e8c8af9c5d6da361b334b4a5e98
ManifoldFR <wilson.jallet@polytechnique.org>
2025-02-20 00:34:15 +0100

    core : add CommandBuffer class

---
+++ src/candlewick/core/CommandBuffer.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "Core.h"
+#include <SDL3/SDL_gpu.h>
+#include <cassert>
+
+namespace candlewick {
+
+class CommandBuffer {
+  SDL_GPUCommandBuffer *_cmdBuf;
+  CommandBuffer(SDL_GPUCommandBuffer *ptr) : _cmdBuf{ptr} {}
+
+public:
+  static CommandBuffer acquire(const Device &device);
+  operator SDL_GPUCommandBuffer *() const { return _cmdBuf; }
+
+  CommandBuffer(const CommandBuffer &) = delete;
+  CommandBuffer(CommandBuffer &&) = delete;
+
+  void submit() { SDL_SubmitGPUCommandBuffer(_cmdBuf); }
+  void cancel() { SDL_CancelGPUCommandBuffer(_cmdBuf); }
+
+  /// \brief Push uniform data to the vertex shader.
+  /// \warning Call this after beginFrame() but **before** endFrame().
+  void pushVertexUniform(Uint32 slot_index, const void *data, Uint32 length) {
+    SDL_PushGPUVertexUniformData(_cmdBuf, slot_index, data, length);
+  }
+  /// \brief Push uniform data to the fragment shader.
+  /// \warning Call this after beginFrame() but **before** endFrame().
+  void pushFragmentUniform(Uint32 slot_index, const void *data, Uint32 length) {
+    SDL_PushGPUFragmentUniformData(_cmdBuf, slot_index, data, length);
+  }
+
+  SDL_GPUFence *submitAndAcquireFence() {
+    return SDL_SubmitGPUCommandBufferAndAcquireFence(_cmdBuf);
+  }
+};
+
+} // namespace candlewick
