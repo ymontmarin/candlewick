04115c58acbbc7fd92bd98fc32d0b7ef2a0df164
ManifoldFR <wilson.jallet@polytechnique.org>
2025-03-12 16:24:00 +0100

    [python] Initialize SDL video subsystem when module imported, quit all subsystems when interpreter shuts down
    + expose some utility functions (query number of GPU drivers, their names, detect shader format)

--- python/src/fwd.hpp
+++ python/src/fwd.hpp
@@ -1,5 +1,4 @@
 #include <eigenpy/eigenpy.hpp>
-#include "candlewick/config.h"
 
 namespace bp = boost::python;
 
@@ -11,12 +10,10 @@ inline std::string get_scope_name(bp::scope scope) {
   return std::string(bp::extract<const char *>(scope.attr("__name__")));
 }
 
-/**
- * @brief   Create or retrieve a Python scope (that is, a class or module
- * namespace).
- *
- * @returns The submodule with the input name.
- */
+/// \brief   Create or retrieve a Python scope (that is, a class or module
+/// namespace).
+///
+/// \returns The submodule with the input name.
 inline bp::object get_namespace(const std::string &name) {
   bp::scope cur_scope; // current scope
   const std::string complete_name = get_scope_name(cur_scope) + "." + name;
--- python/src/module.cpp
+++ python/src/module.cpp
@@ -1,18 +1,39 @@
 #include "fwd.hpp"
+#include "candlewick/config.h"
 #include "candlewick/core/Renderer.h"
 #include "candlewick/core/Shader.h"
 
-namespace bp = boost::python;
+#include <SDL3/SDL_init.h>
+
 using namespace candlewick;
+
+BOOST_PYTHON_FUNCTION_OVERLOADS(adfs_overloads,
+                                auto_detect_shader_format_subset, 0, 1)
 
 #ifdef CANDLEWICK_PYTHON_PINOCCHIO_SUPPORT
 void exposeVisualizer();
 #endif
 void exposeRenderer() {
   bp::class_<Device, boost::noncopyable>("Device", bp::no_init)
-      .def("driverName", &Device::driverName, ("self"_a));
+      .def("driverName", &Device::driverName, ("self"_a))
+      .def("shaderFormats", &Device::shaderFormats);
+
+  bp::def("get_num_gpu_drivers", SDL_GetNumGPUDrivers,
+          "Get number of available GPU drivers.");
+
+  bp::def("get_gpu_driver_name", SDL_GetGPUDriver, ("index"_a),
+          "Get the name of the GPU driver of the corresponding index.");
+
+  bp::def("auto_detect_shader_format_subset", auto_detect_shader_format_subset,
+          adfs_overloads{
+              ("driver_name"_a = NULL),
+              "Automatically detect the compatible set of shader formats."});
+
   bp::class_<Renderer, boost::noncopyable>("Renderer", bp::no_init)
       .def_readonly("device", &Renderer::device);
+
+  // Register SDL_Quit() as a function to call when interpreter exits.
+  Py_AtExit(SDL_Quit);
 }
 
 BOOST_PYTHON_MODULE(pycandlewick) {
@@ -20,6 +41,10 @@ BOOST_PYTHON_MODULE(pycandlewick) {
   bp::scope current_scope;
   current_scope.attr("__version__") = bp::str(CANDLEWICK_VERSION);
 
+  if (!SDL_InitSubSystem(SDL_INIT_VIDEO)) {
+    throw std::runtime_error(std::format(
+        "Failed to initialize SDL subsystems: \'%s\'", SDL_GetError()));
+  }
   ::candlewick::setShadersDirectory(SHADERS_INSTALL_DIR);
   bp::def("currentShaderDirectory", &currentShaderDirectory);
 
