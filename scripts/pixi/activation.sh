b4c249feeb32dbca12f94ddc702c27aed559ec23
ManifoldFR <wilson.jallet@polytechnique.org>
2025-04-03 16:05:45 +0200

    fix activation.sh

--- scripts/pixi/activation.sh
+++ scripts/pixi/activation.sh
@@ -20,6 +20,10 @@ then
   # On GNU/Linux, I don't know if these flags are mandatory with g++ but
   # it allow to use clang++ as compiler
   export LDFLAGS="-Wl,-rpath,$CONDA_PREFIX/lib -Wl,-rpath-link,$CONDA_PREFIX/lib -L$CONDA_PREFIX/lib"
+
+  # Conda compiler is named x86_64-conda-linux-gnu-c++, ccache can't resolve it
+  # (https://ccache.dev/manual/latest.html#config_compiler_type)
+  export CCACHE_COMPILERTYPE=gcc
 fi
 
 # Setup ccache
@@ -34,5 +38,5 @@ export CMAKE_COLOR_DIAGNOSTICS=1
 # Set default build value only if not previously set
 export CANDLEWICK_BUILD_TYPE=${CANDLEWICK_BUILD_TYPE:=Release}
 export CANDLEWICK_WITH_PYTHON=${CANDLEWICK_WITH_PYTHON:=ON}
-export CANDLEWICK_WITH_PINOCCHIO=${CANDLEWICK_WITH_PINOCCHIO:OFF}
-export CANDLEWICK_BUILD_TESTS=${CANDLEWICK_BUILD_TESTS:OFF}
+export CANDLEWICK_WITH_PINOCCHIO=${CANDLEWICK_WITH_PINOCCHIO:=OFF}
+export CANDLEWICK_BUILD_TESTS=${CANDLEWICK_BUILD_TESTS:=OFF}
